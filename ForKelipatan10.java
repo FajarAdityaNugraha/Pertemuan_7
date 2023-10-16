5f4542a69470e79adef230073613dc8c02067c76
FajarAdityaNugraha <fajaradityanugraha01@gmail.com>
2023-10-16 15:21:36 +0700

    Jobsheet 7 pertemuan 7

--- ForKelipatan10.java
+++ ForKelipatan10.java
@@ -7,6 +7,7 @@ public class ForKelipatan10 {
     public static void main(String[] args) {
         Scanner input10 = new Scanner(System.in);
         int kelipatan, jumlah=0, counter=0;
+        double rerata;
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = input10.nextInt();
         for (int i = 1; i <=50; i++) {
@@ -18,6 +19,12 @@ public class ForKelipatan10 {
         }
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        if (counter > 0) {
+            rerata = (double) jumlah / counter;
+            System.out.printf("Rerata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rerata );
+            } else 
+            System.out.println("Rerata bilangan kelipatan " + kelipatan + " dari 1 sampai 50 kosong");
+        
+        }
         
     }
-}
